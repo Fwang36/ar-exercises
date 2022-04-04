962206bc08ccde05ba4f2e8a2bea29583d88d21f
Francis <fwang36@gmail.com>
2022-04-04 17:48:25 -0400

    exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+annual_revenue_sum = Store.sum(:annual_revenue)
+puts annual_revenue_sum
+
+annual_revenue_avg = Store.average(:annual_revenue)
+puts annual_revenue_avg
+
+revenue_store_count = Store.where(annual_revenue: 1000000..Float::INFINITY).count
+
+puts "store count #{revenue_store_count}"
